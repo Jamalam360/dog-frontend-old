b6d02533123f6a180e329a59f1b7f98b574951ef
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-03-09 17:35:25 +0000

    chore: fix packagin

--- backend/constants.ts
+++
@@ -1,7 +0,0 @@
-export const ERROR = "ERORR";
-export const SUCCESS = "SUCCESS";
-export const SUCCESS_CODE = 200;
-export const NOT_FOUND_CODE = 404;
-export const INTERNAL_SERVER_ERROR_CODE = 500;
-export const FORBIDDEN_ERROR_CODE = 403;
-export const UNAUTHORISED_CODE = 401;
