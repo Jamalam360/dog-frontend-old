5723aa3ca0abea2b37e454c5a0cbba02ea9ee5e0
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-03-24 08:50:23 +0000

    wip: index setting fxes

--- util/hooks.ts
+++ util/hooks.ts
@@ -134,6 +134,8 @@ export function useIndex(
 
   useEffect(() => {
     getUserIndex(snowflake).then((number) => {
+      console.log("Fetched state: " + number);
+      console.log("State fetch: " + s);
       if (s != 0) {
         console.log("Setting state: " + number);
         set(number);
